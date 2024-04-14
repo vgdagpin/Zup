cfac4ec5b222f3a15aa0c32e5fb8aa14d9af5157
Vincent Dagpin <vincent.dagpin@live.com>
2024-04-15 06:31:33 +0800

    Test

---
+++ Zup/Entities/ZupDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zup.Entities;
+
+public class ZupDbContext : DbContext
+{
+    public DbSet<tbl_TimeLog> TimeLogs { get; set; }
+
+    public ZupDbContext(DbContextOptions<ZupDbContext> dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+}
